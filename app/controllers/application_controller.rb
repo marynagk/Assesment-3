cf3916ab8ef2c31da26fcf867f008d29a4f90273
Maryna Golovko <mgolovko@gmail.com>
2018-05-06 22:41:06 +1000

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by id: session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def authorize
+    redirect_to "/" if not logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
+  resource  :sessions, only: [ :new, :create, :destroy ]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root 'welcome#index'
-
-   # namespace :slack do
-     get 'login', to: 'session#create'
-  # end
-
-  # get '/auth/:slack/callback', to: 'session#create'
+  root 'welcome#index'
+  get 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
 end
